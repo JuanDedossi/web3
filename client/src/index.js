ef733e227f49105c6dc2f63cf509156d8ed79de2
JuanDedossi <juandedossi@yahoo.com>
2022-01-25 18:22:04 -0300

    minimo css agragado

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,23 @@
-const displayGreeting = async (greeting, contract) => {
+const displayGreeting = async (toDo, contract) => {
   let length = await contract.methods.getLength().call()
   console.log(length)
   $('ul').empty()
   for (let i = 0; i < length; i++) {
-    greeting = await contract.methods.getTask(i).call()
-    console.log(greeting[2])
+    toDo = await contract.methods.getTask(i).call()
+    console.log(toDo)
     $('ul').append(
-      `<li id={${greeting[0]}}><label>${
-        greeting[0]
-      }</label><input type='checkbox' ${
-        greeting[2] ? 'checked' : null
-      } disabled /><label>${greeting[1]}</label></li>`
+      `<li id={${toDo[0]}}><label class="container" id="c3">${toDo[1]}
+      <input type="checkbox" ${
+        toDo[2] ? 'checked' : null
+      } disabled >
+      <span class="checkmark"></span>
+    </label>
+      </li>`
     )
+    
   }
 }
-const updateGreeting = (greeting, contract, accounts) => {
+const updateGreeting = (toDo, contract, accounts) => {
   let input
   $('#input').on('change', e => {
     input = e.target.value
@@ -25,10 +28,10 @@ const updateGreeting = (greeting, contract, accounts) => {
       .addTask(input)
       .send({ from: accounts[0], gas: 400000 })
     $('#input').val('')
-    displayGreeting(greeting, contract)
+    displayGreeting(toDo, contract)
   })
 }
-const updateDone = (greeting, contract, accounts) => {
+const updateDone = (toDo, contract, accounts) => {
   let input
   $('#inputDone').on('change', e => {
     input = e.target.value
@@ -36,19 +39,19 @@ const updateDone = (greeting, contract, accounts) => {
   $('#done').on('submit', async e => {
     e.preventDefault()
     await contract.methods
-      .doneTask(input)
+      .doneTask(input -1)
       .send({ from: accounts[0], gas: 400000 })
     $('#inputDone').val('')
-    displayGreeting(greeting, contract)
+    displayGreeting(toDo, contract)
   })
 }
 async function greetingApp() {
   const web3 = await getWeb3()
   const accounts = await web3.eth.getAccounts()
   const contract = await getContract(web3)
-  let greeting
-  displayGreeting(greeting, contract)
-  updateGreeting(greeting, contract, accounts)
-  updateDone(greeting, contract, accounts)
+  let toDo
+  displayGreeting(toDo, contract)
+  updateGreeting(toDo, contract, accounts)
+  updateDone(toDo, contract, accounts)
 }
 greetingApp()
